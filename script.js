883bce10e4186332460b666534d4b114b11ef00e
Pietro <pietro.martinesmj@gmail.com>
2022-10-11 17:48:56 -0300

    praticando variáveis

---
+++ script.js
@@ -0,0 +1,117 @@
+/*Exercício 1
+
+let valorLitro = 6.80;
+let valorDaCompra = parseFloat(prompt("Digite o Valor da sua compra:"));
+let qntdLitros = valorDaCompra / valorLitro;
+alert(`O cliente abasteceu ${qntdLitros.toFixed(2)} Litros`);
+
+*/
+
+
+
+
+/*Exercício 2
+
+let valorCelsius = parseFloat(prompt("Digite a temperatura em Celsius:"));
+let valorFahrenheit = (9 * valorCelsius + 160)/5;
+alert(`O valor ${valorCelsius}ºC em Fahrenheit é ${valorFahrenheit.toFixed(2)}`);
+
+*/
+
+
+
+
+/*Exercício 3
+
+let valorFahrenheit = parseFloat(prompt("Digite a temperatura em Fahrenheit:"));
+let valorCelsius = ((valorFahrenheit - 32)* 5)/5;
+alert(`O valor ${valorFahrenheit}ºf em Celsius é ${valorCelsius.toFixed(2)}`);
+
+*/
+
+
+
+
+/*Exercício 4
+
+let comprimento = parseFloat(prompt("Digite o comprimento da caixa:"));
+let largura = parseFloat(prompt("Digite a largura da caixa:"));
+let altura = parseFloat(prompt("Digite a altura da caixa"));
+let volume = comprimento * largura * altura;
+alert(`O volume é de: ${volume.toFixed(2)}m³`);
+
+*/
+
+
+
+
+/*Exercício 5
+
+let inteiro = parseInt(prompt("Digite um número inteiro:"));
+let quadrado = inteiro ** 2;
+alert(`O valor ${inteiro} ao quadrado é ${quadrado}`);
+
+*/
+
+
+
+
+/*Exercécio 6
+
+let nUm =  parseFloat(prompt("Digite a primeira nota:"));
+let nDois =  parseFloat(prompt("Digite a segunda nota:"));
+let nTres =  parseFloat(prompt("Digite a terceira nota:"));
+let nQuatro =  parseFloat(prompt("Digite a quarta nota:"));
+let nCinco =  parseFloat(prompt("Digite a quinta nota:"));
+let nSeis =  parseFloat(prompt("Digite a sexta nota:"));
+let nSete =  parseFloat(prompt("Digite a sétima nota:"));
+let soma = nUm + nDois + nTres + nQuatro + nCinco + nSeis + nSete;
+let media = soma / 7;
+alert(`A soma dos valores informados é: ${soma} e a média das notas é ${media.toFixed(2)}.`);
+
+*/
+
+
+
+
+/*Exercício 7
+
+let a = parseInt(prompt("Digite um número inteiro"));
+let b = parseInt(prompt("Digite um número inteiro"));
+let resto = a % b;
+alert(`O resto da divisão de ${a} por ${b} é: ${resto}`);
+
+*/
+
+
+
+
+/*Exercício 8
+
+let palavra = prompt("Digite uma palavra:");
+let qntd = palavra.length;
+alert(`Este texto tem ${qntd} caracteres`);
+
+*/
+
+
+
+/*Exercício 9
+
+let texto = prompt("Digite uma palavra:");
+let primeiraLetra = texto[0];
+let ultimaLetra = texto[texto.length - 1];
+alert (`A primeira letra deste texto é ${primeiraLetra.toUpperCase()} e última é ${ultimaLetra.toLowerCase()}`);
+
+*/
+
+
+
+/*Exercício 10
+
+let nome = prompt("Digite seu nome e sobrenome");
+let idade = parseInt(prompt("Digite sua idade:"));
+let setor = prompt("Digite seu Setor:");
+alert(`nome: ${nome.toUpperCase()} - idade: ${idade} - Setor: ${setor}`);
+
+*/
